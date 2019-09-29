ffc94e572d5adbbabb94f4185a6f21328ddfba3b
odessit007 <igor.vustianiuk@emedgene.com>
2019-09-29 17:20:51 +0300

    change url

--- github_bot/__main__.py
+++ github_bot/__main__.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     run_app(
         name='PyCon-Bot-by-webknjaz',
         version='1.0.0',
-        url='https://github.com/apps/pyyyyyycoooon-booooot111',
+        url='https://github.com/apps/my-first-github-bot-007',
     )
